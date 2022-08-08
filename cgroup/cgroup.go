b9db17602af551535b914bdb33926ddfccfdd15a
Stefan Agner <stefan@agner.ch>
2022-08-08 22:04:28 +0200

    Support Device CGroup rules update through runc (#92)
    * Support Device CGroup rules update through runc
    
    With CGroups v2, device CGroup rules update is not as straight forward
    since it requires ebpf rules. Currently, runc does support updating
    this ebpf rules, but when using the runc update command new device
    rules are not passed yet. A patched runc version (and hopefully future
    runc versions) support updating device resources, see:
    https://github.com/opencontainers/runc/pull/3402
    
    Use this (patched) runc capability to support Device CGroup rules
    updates with CGroups v2.
    
    * Determine CGroup version at initialization time
    
    * Address go linter issues

--- cgroup/cgroup.go
+++ cgroup/cgroup.go
@@ -1,8 +1,10 @@
 package cgroup
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	securejoin "github.com/cyphar/filepath-securejoin"
@@ -19,42 +21,95 @@ const (
 	cgroupFSDockerDevices = "/sys/fs/cgroup/devices/docker"
 )
 
+type CGroupVersion int
+
+const (
+	CGroupUnknown CGroupVersion = 0
+	CGroupV1
+	CGroupV2
+)
+
 type cgroup struct {
-	conn *dbus.Conn
+	conn          *dbus.Conn
+	cgroupVersion CGroupVersion
 }
 
 func (d cgroup) AddDevicesAllowed(containerID string, permission string) (bool, *dbus.Error) {
-	// Make sure path is relative to cgroupFSDockerDevices
-	allowedFile, err := securejoin.SecureJoin(cgroupFSDockerDevices, containerID+string(filepath.Separator)+"devices.allow")
-	if err != nil {
-		return false, dbus.MakeFailedError(fmt.Errorf("Security issues with '%s': %s", containerID, err))
-	}
+	if d.cgroupVersion == CGroupV2 {
+		permissions := []string{permission}
+		resources, err := CreateDeviceUpdateResources(permissions)
+		if err != nil {
+			error := fmt.Errorf("Error calling runc for '%s': %s", containerID, err)
+			logging.Error.Printf("%s", error)
+			return false, dbus.MakeFailedError(error)
+		}
 
-	// Check if file/container exists
-	_, err = os.Stat(allowedFile)
-	if os.IsNotExist(err) {
-		return false, dbus.MakeFailedError(fmt.Errorf("Can't find Container '%s' for adjust CGroup devices.", containerID))
-	}
+		fmt.Printf("Container resources update %#v\n", resources)
 
-	// Write permission adjustments
-	file, err := os.Create(allowedFile)
-	if err != nil {
-		return false, dbus.MakeFailedError(fmt.Errorf("Can't open CGroup devices '%s': %s", allowedFile, err))
-	}
-	defer file.Close()
+		cmd := exec.Command("runc", "--root", "/var/run/docker/runtime-runc/moby/", "update", "--resources", "-", containerID)
+		fmt.Printf("Calling command %v", cmd.Args)
 
-	_, err = file.WriteString(permission + "\n")
-	if err != nil {
-		return false, dbus.MakeFailedError(fmt.Errorf("Can't write CGroup permission '%s': %s", permission, err))
-	}
+		// Pass resources as OCI LinuxResources JSON object
+		stdin, err := cmd.StdinPipe()
+		if err != nil {
+			error := fmt.Errorf("Error calling runc for '%s': %s", containerID, err)
+			logging.Error.Printf("%s", error)
+			return false, dbus.MakeFailedError(error)
+		}
+		enc := json.NewEncoder(stdin)
+		enc.Encode(resources)
+		stdin.Close()
 
-	logging.Info.Printf("Permission '%s', granted for Container '%s'", containerID, permission)
-	return true, nil
+		stdoutStderr, err := cmd.CombinedOutput()
+		if err != nil {
+			error := fmt.Errorf("Error calling runc for '%s': %s, output %s", containerID, err, stdoutStderr)
+			logging.Error.Printf("%s", error)
+			return false, dbus.MakeFailedError(error)
+		}
+
+		logging.Info.Printf("Permission '%s', granted for Container '%s'", containerID, permission)
+		return true, nil
+	} else {
+		// Make sure path is relative to cgroupFSDockerDevices
+		allowedFile, err := securejoin.SecureJoin(cgroupFSDockerDevices, containerID+string(filepath.Separator)+"devices.allow")
+		if err != nil {
+			return false, dbus.MakeFailedError(fmt.Errorf("Security issues with '%s': %s", containerID, err))
+		}
+
+		// Check if file/container exists
+		_, err = os.Stat(allowedFile)
+		if os.IsNotExist(err) {
+			return false, dbus.MakeFailedError(fmt.Errorf("Can't find Container '%s' for adjust CGroup devices.", containerID))
+		}
+
+		// Write permission adjustments
+		file, err := os.Create(allowedFile)
+		if err != nil {
+			return false, dbus.MakeFailedError(fmt.Errorf("Can't open CGroup devices '%s': %s", allowedFile, err))
+		}
+		defer file.Close()
+
+		_, err = file.WriteString(permission + "\n")
+		if err != nil {
+			return false, dbus.MakeFailedError(fmt.Errorf("Can't write CGroup permission '%s': %s", permission, err))
+		}
+
+		logging.Info.Printf("Permission '%s', granted for Container '%s'", containerID, permission)
+		return true, nil
+	}
 }
 
 func InitializeDBus(conn *dbus.Conn) {
 	d := cgroup{
-		conn: conn,
+		conn:          conn,
+		cgroupVersion: CGroupUnknown,
+	}
+
+	// Check for CGroups v2
+	if _, err := os.Stat("/sys/fs/cgroup/cgroup.controllers"); err == nil {
+		d.cgroupVersion = CGroupV2
+	} else {
+		d.cgroupVersion = CGroupV1
 	}
 
 	err := conn.Export(d, objectPath, ifaceName)
